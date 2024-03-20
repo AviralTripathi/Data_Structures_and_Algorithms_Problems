6889b867e0db607c4e07338b5168d353a28863f2
AviralTripathi <111529523+AviralTripathi@users.noreply.github.com>
2024-03-21 01:45:43 +0530

    Create merge2SortedLists.cpp

---
+++ Linked List/merge2SortedLists.cpp
@@ -0,0 +1,52 @@
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) {
+        if (head1 == NULL)
+            return head2;
+        if (head2 == NULL)
+            return head1;
+        // creating a dummy node
+        ListNode* result = NULL;
+        if (head1->val < head2->val) {
+            result = head1;
+            result->next = mergeTwoLists(head1->next, head2);
+        } else {
+            result = head2;
+            result->next = mergeTwoLists(head1, head2->next);
+        }
+        return result;
+    }
+};
+
+Complexity Analysis:
+
